db1e8a588c615bf801f81ab98f9f9bf11ca27dc7
hernan verar <hernanverar@gmail.com>
2023-08-18 08:53:15 -0700

    This commit add Restaurant Models database

---
+++ Models/LocalBusinessContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalBusinessApi.Models
+{
+  public class LocalBusinessContext : DbContext
+  {
+    public DbSet<Restaurant> Restaurants { get; set; }
+
+    public LocalBusinessApiContext(DbContextOptions<LocalBusinessApiContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Restaurant>()
+        .HasData(
+          new Restaurant { RestaurantId = 1, Name = "RedWood" },
+          new Restaurant { RestaurantId = 2, Name = "Woods Man" },
+          new Restaurant { RestaurantId = 3, Name = "Wayan" },
+          new Restaurant { RestaurantId = 4, Name = "Sweet Basil" },
+          new Restaurant { RestaurantId = 5, Name = "Los Gorditos" }
+        );
+    }    
+  }
+}    
